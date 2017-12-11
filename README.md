7c0e996c35b8c5eda87f23b7074abae4f43ed4d2
Isabel Hasse <isabelhasse2@gmail.com>
2017-12-10 18:08:18 -0800

    create form to order mulitple pizzas and display total cost of order.

---
+++ README.md
@@ -0,0 +1,3 @@
+Specs:
+  -Allows user to choose size of pizza and displays price accordingly.
+  -Allows user to choose toppings on pizza and displays price accordingly.
---
+++ js/scripts.js
@@ -0,0 +1,37 @@
+function Pizza(size, toppings) {
+  this.size = size;
+  this.toppings = toppings;
+};
+
+Pizza.prototype.price = function() {
+  var price = this.size;
+  this.toppings.each(function() {
+    var topping = parseInt( $(this).val() );
+    price += topping;
+  });
+  return price;
+};
+
+$(document).ready(function(){
+  var pizzas = [];
+  var counter = 2;
+
+  $("#order-form").submit(function(event){
+    event.preventDefault();    
+
+    var orderedSize = parseInt( $("input:radio[name=size]:checked").val() );
+    var orderedToppings = $("input:checkbox[name=toppings]:checked");
+    var orderedPizza = new Pizza(orderedSize, orderedToppings);
+    pizzas.push(orderedPizza);
+
+    var totalPrice = 0;
+    pizzas.forEach(function(pizza) {
+      totalPrice += pizza.price();
+    });
+
+    $("#number").text(counter);
+    $("#price").text("Your total is $" + totalPrice);
+
+    counter += 1;
+  });
+});
